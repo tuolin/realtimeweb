0be4ca438ad7ac94a887384337fdc02b98ce8dd8
Tuolin Chen <tuolin@gmail.com>
2011-06-12 23:23:09 +0800

    added draw color support

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
  */
 
 // Command Constants
-var IDLE = 0, LOGIN = 1, USERID = 2, DRAW = 4;
+var IDLE = 0, LOGIN = 1, USERID = 2, DRAW = 4,
+	CHANGECOLOR = 8, CHANGESHAPE = 16, LOGOUT = 32,
+	STARTDRAWING = 64, SYNCUSER = 128;
 
 var express = require('express');
 var ws = require("websocket-server")
@@ -12,7 +14,7 @@ var ws = require("websocket-server")
 var app = module.exports = express.createServer();
 
 var nextUserId = 1;
-var users = new Array();
+var users = {};
 
 // Configuration
 
@@ -52,9 +54,35 @@ wsserver.addListener("connection", function(connection) {
 	});
 });
 
+function User(userId) {
+	this.drawing = false;
+	this.userId = userId;
+	this.oldX = undefined;
+	this.oldY = undefined;
+	this.color = undefined;
+	this.shape = undefined;
+	
+	this.setDrawing = function(d) {
+		this.drawing = d;
+	}
+	
+	this.save = function(x, y) {
+		this.oldX = x;
+		this.oldY = y;
+	}
+	
+	this.setColor = function(c) {
+		this.color = c;
+	}
+	
+	this.setShape = function(s) {
+		this.shape = s;
+	}
+}
+
 function wsOnMessage(con, msg) {
 	console.log("got message: " + msg);
-	var cmd = msg.split(',');
+	var cmd = msg.split('|');
 	switch (parseInt(cmd[0]))
 	{
 		case IDLE:
@@ -65,6 +93,15 @@ function wsOnMessage(con, msg) {
 			onLogin(con);
 			break;
 		case DRAW:
+			users[cmd[1]].save(cmd[2], cmd[3]);
+			con.broadcast(msg);
+			break; 
+		case CHANGECOLOR:
+			users[cmd[1]].setColor(cmd[2]);
+			con.broadcast(msg);
+			break; 
+		case STARTDRAWING:
+			users[cmd[1]].save(cmd[2], cmd[3]);
 			con.broadcast(msg);
 			break; 
 	}
@@ -72,10 +109,23 @@ function wsOnMessage(con, msg) {
 
 function onLogin(con) {
 	var id = nextUserId++;
-	var user = {id:id, con:con};
-	users.push(user);
-	var str = USERID + ',' + id;
+	var user = new User(id);
+	users[id] = user;
+	var str = [USERID,id].join('|');
 	con.send(str);
+	
+	// let other client know a new user has joined
+	str = [LOGIN, id].join('|');
+	con.broadcast(str);
+	
+	// synchronize other clients that joined before this connection
+	for (var k in users) {
+		if (k != id && users.hasOwnProperty(k)) {
+			var u = users[k];
+			var msg = [SYNCUSER, u.userId, u.oldX, u.oldY, u.color, u.shape].join('|');
+			con.send(msg);
+		}
+	}
 }
 
 wsserver.listen(8080);
